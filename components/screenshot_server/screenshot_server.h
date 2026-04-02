a446a8ebfc7a5048565ff3932a7fcfaf4ae9fab5
SleepinDevil <sleepindevil@hotmail.com>
2026-04-02 22:37:33 +1100

    Update screenshot_server.h

--- components/screenshot_server/screenshot_server.h
+++ components/screenshot_server/screenshot_server.h
@@ -1,25 +1,28 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/display/display_buffer.h"
 #include "esphome/components/web_server_base/web_server_base.h"
+#include <functional>
 
 namespace esphome {
 namespace screenshot_server {
 
 class ScreenshotServer : public Component {
  public:
-  void set_display(display::DisplayBuffer *display) { display_ = display; }
-  void set_web_server(web_server_base::WebServerBase *web_server) { web_server_ = web_server; }
+  // Accept ANY display class without caring about its inheritance tree
+  template <typename T> void set_display(T *display) {
+    this->width_func_ = [display]() -> int { return display->get_width(); };
+    this->height_func_ = [display]() -> int { return display->get_height(); };
+    this->buffer_func_ = [display]() -> uint8_t* { return (uint8_t*)display->get_buffer(); };
+  }
 
   void setup() override;
-  
-  // Set setup priority to load after the web server and display initialize
   float get_setup_priority() const override { return setup_priority::LATE; }
 
  protected:
-  display::DisplayBuffer *display_{nullptr};
-  web_server_base::WebServerBase *web_server_{nullptr};
+  std::function<int()> width_func_;
+  std::function<int()> height_func_;
+  std::function<uint8_t*()> buffer_func_;
 };
 
 }  // namespace screenshot_server
