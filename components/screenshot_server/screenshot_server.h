224688bc77423bcbc81625070cbd30e9cc1e2612
SleepinDevil <sleepindevil@hotmail.com>
2026-04-02 23:53:55 +1100

    Update screenshot_server.h

--- components/screenshot_server/screenshot_server.h
+++ components/screenshot_server/screenshot_server.h
@@ -1,31 +1,20 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/display/display_buffer.h"
 #include "esphome/components/web_server_base/web_server_base.h"
-#include "esphome/components/st7701s/st7701s.h"
 
 namespace esphome {
 namespace screenshot_server {
 
-// MAGIC TRICK: This bypasses C++ access modifiers to unlock the protected hardware handle
-class ST7701SAccessor : public st7701s::ST7701S {
-public:
-  esp_lcd_panel_handle_t get_handle() {
-    return this->panel_handle_;
-  }
-};
-
 class ScreenshotServer : public Component {
  public:
-  // Accept the component and cast it to the ST7701S type
-  template <typename T> void set_display(T *display) { 
-    display_ = (st7701s::ST7701S*)display; 
-  }
+  void set_display(display::DisplayBuffer *display) { display_ = display; }
 
   void setup() override;
   float get_setup_priority() const override { return setup_priority::LATE; }
 
-  st7701s::ST7701S *display_{nullptr};
+  display::DisplayBuffer *display_{nullptr};
 };
 
 }  // namespace screenshot_server
