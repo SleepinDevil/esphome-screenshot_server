e2cd888fd8b9486789813434a81134ea30b27ad3
SleepinDevil <sleepindevil@hotmail.com>
2026-04-02 22:51:27 +1100

    Update __init__.py

--- components/screenshot_server/__init__.py
+++ components/screenshot_server/__init__.py
@@ -10,16 +10,15 @@ ScreenshotServer = screenshot_server_ns.class_('ScreenshotServer', cg.Component)
 
 CONF_DISPLAY_ID = 'display_id'
 
-# Relaxed Schema: Accept ANY component ID and let C++ figure it out
+# Validate against the generic Component class
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ScreenshotServer),
-    cv.Required(CONF_DISPLAY_ID): cv.use_id(), 
+    cv.Required(CONF_DISPLAY_ID): cv.use_id(cg.Component), 
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    # Blindly pass the ID to C++
     disp = await cg.get_variable(config[CONF_DISPLAY_ID])
     cg.add(var.set_display(disp))
