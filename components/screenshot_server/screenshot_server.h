58442364551130cfaa027c36c40cd9ea0472d323
SleepinDevil <sleepindevil@hotmail.com>
2026-04-02 23:37:07 +1100

    Update screenshot_server.h

--- components/screenshot_server/screenshot_server.h
+++ components/screenshot_server/screenshot_server.h
@@ -2,26 +2,30 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/web_server_base/web_server_base.h"
-#include <functional>
+#include "esphome/components/st7701s/st7701s.h"
 
 namespace esphome {
 namespace screenshot_server {
 
+// MAGIC TRICK: This bypasses C++ access modifiers to unlock the protected hardware handle
+class ST7701SAccessor : public st7701s::ST7701S {
+public:
+  esp_lcd_panel_handle_t get_handle() {
+    return this->panel_handle_;
+  }
+};
+
 class ScreenshotServer : public Component {
  public:
-  template <typename T> void set_display(T *display) {
-    this->width_func_ = [display]() -> int { return display->get_width(); };
-    this->height_func_ = [display]() -> int { return display->get_height(); };
-    this->buffer_func_ = [display]() -> uint8_t* { return (uint8_t*)display->get_buffer(); };
+  // Accept the component and cast it to the ST7701S type
+  template <typename T> void set_display(T *display) { 
+    display_ = (st7701s::ST7701S*)display; 
   }
 
   void setup() override;
   float get_setup_priority() const override { return setup_priority::LATE; }
 
-  // Moved to public so the WebHandler can read them
-  std::function<int()> width_func_;
-  std::function<int()> height_func_;
-  std::function<uint8_t*()> buffer_func_;
+  st7701s::ST7701S *display_{nullptr};
 };
 
 }  // namespace screenshot_server
